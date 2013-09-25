ee3b3e2ea087cd201e7e9d33d35bdbad84a914ae
Charles Blonde <cblonde@xebia.fr>
2013-09-25 22:21:59 +0200

    Update Monitoring and AppDynamics params

--- infrastructure/prod/tomcat-prod/src/main/catalina_base/bin/setenv.sh
+++ infrastructure/prod/tomcat-prod/src/main/catalina_base/bin/setenv.sh
@@ -7,7 +7,8 @@ export CATALINA_BASE CATALINA_HOME CATALINA_PID;
 CATALINA_OPTS="-Xmx256m -XX:MaxPermSize=128m"
 
 #CATALINA_NEW_RELIC_OPTS="-Dnewrelic.environment=production -javaagent:$CATALINA_BASE/lib/newrelic-2.10.0.jar"
-CATALINA_APPDYNAMICS="-javaagent:/opt/appdynamics/javaagent.jar -Dappdynamics.agent.applicationName=TRAINING-DEVOPS -Dappdynamics.agent.tierName=WEB -Dappdynamics.agent.nodeName=JVM-PROD -Dappdynamics.controller.hostName=xebia-lab.saas.appdynamics.com -Dappdynamics.controller.port=80 -Dappdynamics.agent.accountName=xebia-lab -Dappdynamics.agent.accountAccessKey=7fb9c37ae8e6 "
+JVMNAME=`hostname |tr '[:lower:]' '[:upper:]'`
+CATALINA_APPDYNAMICS="-javaagent:/opt/appdynamics/javaagent.jar -Dappdynamics.agent.applicationName=TRAINING-DEVOPS -Dappdynamics.agent.tierName=WEB -Dappdynamics.agent.nodeName=$JVMNAME -Dappdynamics.controller.hostName=xebia-lab.saas.appdynamics.com -Dappdynamics.controller.port=80 -Dappdynamics.agent.accountName=xebia-lab -Dappdynamics.agent.accountAccessKey=7fb9c37ae8e6 "
 
 CATALINA_JMX_OPTS="\
     -Dcom.sun.management.jmxremote \
