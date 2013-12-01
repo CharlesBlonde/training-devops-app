43e3683a1ae1084a0e6389b61f2efc72a015abcc
Charles Blonde <cblonde@xebia.fr>
2013-12-01 11:03:01 +0100

    Add AppDynamics properties

--- infrastructure/prod/tomcat-prod/src/main/catalina_base/bin/setenv.sh
+++ infrastructure/prod/tomcat-prod/src/main/catalina_base/bin/setenv.sh
@@ -1,14 +1,19 @@
 CATALINA_BASE=/home/ec2-user/tomcat
 CATALINA_HOME=/opt/tomcat/tomcat6
 CATALINA_PID=$CATALINA_BASE/tomcat.pid
+APPDYNAMICS_DIR=/opt/appdynamics
 
 export CATALINA_BASE CATALINA_HOME CATALINA_PID;
 
 CATALINA_OPTS="-Xmx256m -XX:MaxPermSize=128m"
 
 #CATALINA_NEW_RELIC_OPTS="-Dnewrelic.environment=production -javaagent:$CATALINA_BASE/lib/newrelic-2.10.0.jar"
-JVMNAME=`hostname |tr '[:lower:]' '[:upper:]'`
-CATALINA_APPDYNAMICS="-javaagent:/opt/appdynamics/javaagent.jar -Dappdynamics.agent.applicationName=TRAINING-DEVOPS -Dappdynamics.agent.tierName=WEB -Dappdynamics.agent.nodeName=$JVMNAME -Dappdynamics.controller.hostName=xebia-lab.saas.appdynamics.com -Dappdynamics.controller.port=80 -Dappdynamics.agent.accountName=xebia-lab -Dappdynamics.agent.accountAccessKey=7fb9c37ae8e6 "
+CATALINA_APPDYNAMICS=""
+if [ -d ${APPDYNAMICS_DIR} ]
+then
+    JVMNAME=`hostname |tr '[:lower:]' '[:upper:]'`
+    CATALINA_APPDYNAMICS="-javaagent:/opt/appdynamics/javaagent.jar -Dappdynamics.agent.logs.dir=${CATALINA_BASE}/logs -Dappdynamics.agent.applicationName=TRAINING-DEVOPS -Dappdynamics.agent.tierName=WEB -Dappdynamics.agent.nodeName=$JVMNAME -Dappdynamics.controller.hostName=xebia-lab.saas.appdynamics.com -Dappdynamics.controller.port=80 -Dappdynamics.agent.accountName=xebia-lab -Dappdynamics.agent.accountAccessKey=7fb9c37ae8e6 "
+fi
 
 CATALINA_JMX_OPTS="\
     -Dcom.sun.management.jmxremote \
